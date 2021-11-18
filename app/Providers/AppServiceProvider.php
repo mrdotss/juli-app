eb036fc9fb6d2c1109d594fb4e68a97368e77a0c
mrdotss <mayer@fatechid.com>
2021-11-19 03:30:47 +0700

    Fix register in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('path.public',function(){
+            return'/home/fatechi3/public_html'; 
+            });
     }
 
     /**
